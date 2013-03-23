0e87bab256e7c84acb1e3294965cce4ac31ab052
Kartik Vedalaveni <bytecar@gmail.com>
2013-03-23 19:02:01 +0530

    Initial Commit

---
+++ CoScheduler/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CoScheduler
+//
+//  Created by Kartik Vedalaveni on 3/23/13.
+//  Copyright (c) 2013 Kartik Vedalaveni. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
